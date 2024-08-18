c26d3b33f75e471e162a3495be249d5084ad9ca7
Haidere1 <syedfaizanhaider.fh@gmail.com>
2024-08-18 19:28:05 +0500

    App.js

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -6,18 +6,14 @@ import BasicExample from './Components/Home/MenuPage.js';
 import CollapsibleExample from './Components/Home/tabbar.js';
 import { Route,Routes } from 'react-router-dom';
 import ViewProduct from './Components/Products/viewproduct.js';
-import  {AddProduct}  from './Backend/CRUD/addproduct.js';
-import AdminPanel from './Backend/CRUD/udpanel.js';
-import GetProduct from './Backend/CRUD/getproduct.js';
 import Cart from './Components/Cart/cart.js';
 
 function App() {
   
   return (
 <Routes>
-  <Route path='/' element={<AddProduct/>}></Route>
+  
   <Route path='/home' element={<Home/>}></Route>
-  <Route path='/admin/update' element={<AdminPanel/>}></Route>
   <Route path='/menu' element={<BasicExample/>}></Route>
   <Route path='/aboutus' element={<AboutUs/>}></Route>
   <Route path='/cart' element={<Cart/>}></Route>
